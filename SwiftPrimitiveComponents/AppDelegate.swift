3859b48ad60588e9d562084ddb183463adfb3189
Devin Abbott <devinabbott@gmail.com>
2018-04-21 09:34:30 -0700

    Initial Commit

---
+++ SwiftPrimitiveComponents/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftPrimitiveComponents
+//
+//  Created by devin_abbott on 4/21/18.
+//  Copyright © 2018 devin_abbott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
